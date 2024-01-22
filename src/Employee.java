90f90626dc7c9bac7f57027fddb695be1c6178d3
RAAGUL VIGNESH <rv0815@srmist.edu.in>
2024-01-22 11:34:46 +0530

    [Raagul] Update : Updated a Usecase [uc2]

--- src/Employee.java
+++ src/Employee.java
@@ -1,11 +1,17 @@
 public class Employee {
     String name;
     Integer id;
+
+    Integer wageperhour;
+    Integer workhour = 0;
+
     Boolean attendance;
-    Employee(String n,Integer i){
+    Employee(String n,Integer i,Integer w){
         this.name = n;
         this.id = i;
+        this.wageperhour = w;
     }
+
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,37 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Employee e1 = new Employee("Raagul",88);
+        Employee e1 = new Employee("Raagul",88,10);
         System.out.println(e1);
+
         // Using Random function to generate the attendance
         int check = (int)((Math.random()*10))%2;
+
         // Condition for the attendance
         if(check == 0){
             e1.attendance = Boolean.FALSE;
-
         }
         else {
             e1.attendance = Boolean.TRUE;
         }
+
         // Check weather employee is present or not
         if(e1.attendance){
             System.out.println("Employee is present");
+            e1.workhour = 8;
         }
         else {
             System.out.println("Employee is absent");
         }
 
+        Main m1 = new Main();
+        System.out.println(m1.calcsal(e1));
+
+
+
+    }
+    // Function for Calculating the salary of the employee
+    double calcsal(Employee e){
+        return e.workhour*e.wageperhour;
     }
 }
