f1e25a87f3e289cde42a911da4bf92c29316579b
RAAGUL VIGNESH <rv0815@srmist.edu.in>
2024-01-22 16:06:33 +0530

    [Raagul] Update : Updated a Usecase [UC6]

--- src/Employee.java
+++ src/Employee.java
@@ -2,15 +2,15 @@ public class Employee {
     String name;
     Integer id;
 
-    Integer wageperhour;
-    Integer workhour = 0;
-    Integer noofdayspresent = 0;
+    Integer wagePerHour;
+    Integer workHour = 0;
+    Integer totalWorkHours = 0;
 
     Integer attendance;
     Employee(String n,Integer i,Integer w){
         this.name = n;
         this.id = i;
-        this.wageperhour = w;
+        this.wagePerHour = w;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,41 @@
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Employee e1 = new Employee("Aryan", 1, 200);
 
-        Employee e1 = new Employee("Raagul",88,10);
+        int totalDaysWorked = 0;
+        while (e1.totalWorkHours < 100 && totalDaysWorked < 20) {
+            int check = (int) ((Math.random() * 10)) % 3;
 
+            if (check == 0) {
+                e1.attendance = 0;
+            } else if (check == 1) {
+                e1.attendance = 1;
+            } else {
+                e1.attendance = 2;
+            }
 
-        // Using Random function to generate the attendance
-        int check = (int)((Math.random()*10))%3;
-
-        // Condition for the attendance
-        if(check == 0){
-            e1.attendance = 0;
+            switch (e1.attendance) {
+                case 1:
+                    System.out.println("Employee is present");
+                    e1.workHour = 8;
+                    break;
+                case 2:
+                    System.out.println("Employee is absent");
+                    e1.workHour = 0;
+                    break;
+                default:
+                    System.out.println("Employee is part-time");
+                    e1.workHour = 4;
+            }
+            e1.totalWorkHours += e1.workHour;
+            totalDaysWorked++;
+            System.out.println("Day " + totalDaysWorked + " - Total Work Hours: " + e1.totalWorkHours);
         }
-        else if(check == 1) {
-            e1.attendance = 1;
-        }
-        else {
-            e1.attendance = 2;
-        }
-
-        // Check weather employee is present or not
-        switch (e1.attendance) {
-            case 1:
-                System.out.println("Employee is present");
-                e1.workhour = 8;
-                break;
-            case 2:
-                System.out.println("Employee is absent");
-                break;
-            default:
-                System.out.println("Employee is part time");
-                e1.workhour = 4;
-        }
-
         Main m1 = new Main();
-        System.out.println("Salary for the employee is : "+m1.calcsal(e1));
-        System.out.println("Monthly wage of the employee is : "+m1.calcmonthly(e1));
+        System.out.println("Total Work Hours : "+e1.totalWorkHours);
+        System.out.println("Total Salary: " + m1.calculateSalary(e1));
     }
-
-    // Function for Calculating the salary of the employee
-    double calcsal(Employee e){
-        return e.workhour*e.wageperhour;
-    }
-    double calcmonthly(Employee e){
-        return 20*calcsal(e);
+    double calculateSalary(Employee e){
+        return e.totalWorkHours * e.wagePerHour;
     }
 }
