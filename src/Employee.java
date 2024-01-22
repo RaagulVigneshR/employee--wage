64ec8c8b105210b32c1b90a932c14e363c051032
RAAGUL VIGNESH <rv0815@srmist.edu.in>
2024-01-22 15:40:31 +0530

    [Raagul] Update : Updated a Usecase [testuc]

--- src/Employee.java
+++ src/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
 
     Integer wageperhour;
     Integer workhour = 0;
+    Integer noofdayspresent = 0;
 
     Integer attendance;
     Employee(String n,Integer i,Integer w){
